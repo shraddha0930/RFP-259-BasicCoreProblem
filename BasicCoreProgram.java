78d7d2aa37bf0b384bec9154268ba72620bebea3
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-25 18:43:47 +0530

    Passed: welcome message

---
+++ BasicCoreProgram.java
@@ -0,0 +1,10 @@
+package assignments.CorejavaAndFunctionalprogram;
+
+
+
+public class BasicCoreProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Basic Core Programs");
+
+    }
+}
